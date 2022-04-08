ab9beea898269840bac8a94b158cadc909988695
DavidYareth <davidyareth@gmail.com>
2022-04-08 17:11:16 +0200

    update

--- movies-service-develop/app/src/main/kotlin/es/usj/androidapps/services/GenreServiceApi.kt
+++
@@ -1,7 +0,0 @@
-package es.usj.androidapps.services
-
-import es.usj.androidapps.model.dto.GenreDTO
-import org.springframework.stereotype.Service
-
-@Service
-interface GenreServiceApi : IServiceApi<GenreDTO>
